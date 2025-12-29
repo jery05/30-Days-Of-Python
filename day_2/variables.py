733f346126b376904757fc70402f677f97d6ec5b
jery05 <96930553+jery05@users.noreply.github.com>
2025-12-29 16:50:10 +0545

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,28 @@
+#'Day2: 30 Days of python programming'
+
+first_name = 'Adarsha'
+last_name = 'Neupane'
+full_name = 'Adarsha Neupane'
+country = 'Nepal'
+city = 'Biratnagar'
+age = 19
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+linux lfs = 'Greatest OS for learning' True
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+type(linux)
+type(lfs)
+len(first_name)
+print("difference in lecgth is" len(first_name) - len(last_name))
 
